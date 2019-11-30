c046ddac84603334c002083e727420ae27217ea3
Dillon Franklin <drfranklin1331@gmail.com>
2019-11-30 19:00:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,11 @@ def using_pop(array)
   array.pop
 end
 
+def pop_with_args(array)
+  array.pop(2)
+end
+
+
 def using_contact(array, array2)
   array.contact.array2
 end
@@ -42,9 +47,6 @@ end
 
 
 
-def pop_with_args(array)
-  array.pop(2)
-end
 
 def using_shift(array)
   array.shift 
