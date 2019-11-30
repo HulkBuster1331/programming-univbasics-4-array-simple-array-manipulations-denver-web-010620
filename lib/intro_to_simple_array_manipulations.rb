d5e3202c447166cc26bf39eb2b1fec0059ee49fb
Dillon Franklin <drfranklin1331@gmail.com>
2019-11-30 18:35:22 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(array,string)
+  array.push("violet")
+end
+
+def using_contact(array, array2)
+  array.contact.array2
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+
+def using_uniq(array)
+  array.uniq
+end
+
+
+def using_flatten(array)
+  array.flatten
+end
+
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+
+def using_delete_at(array, integer)
+  array.delete_at(2)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def using_pop_with_args(array)
+  array.pop_with_args[2]
+end
