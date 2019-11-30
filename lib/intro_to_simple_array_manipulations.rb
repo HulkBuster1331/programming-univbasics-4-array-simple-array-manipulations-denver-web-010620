169e5bdc28748d6b56d08967d66aec6c2a0db6bb
Dillon Franklin <drfranklin1331@gmail.com>
2019-11-30 19:01:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,6 +18,11 @@ def using_shift(array)
   array.shift 
 end
 
+def shift_with_args(array)
+  array.shift(2)
+end
+
+
 def using_contact(array, array2)
   array.contact.array2
 end
@@ -53,9 +58,6 @@ end
 
 
 
-def shift_with_args(array)
-  array.shift(2)
-end
 
 def using_concat(array, array2)
  array.concat(array, array2)
